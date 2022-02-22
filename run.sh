2b2087e3df7c3ee0cf95519cea290dd6d3c976dd
JorgeSintes <jorge.sintes@gmail.com>
2022-02-22 13:48:07 +0100

    Things for the HPC

--- run.sh
+++ run.sh
@@ -1,27 +1,25 @@
-### -- specify queue -- 
-#BSUB -q hpc
-### -- set the job Name -- 
-#BSUB -J CNN
-### -- ask for number of cores (default: 1) -- 
-#BSUB -n 2 
-### -- specify that the cores must be on the same host -- 
-#BSUB -R "span[hosts=1]"
-### -- specify that we need 2GB of memory per core/slot -- 
-#BSUB -R "rusage[mem=2GB]"
-### -- specify that we want the job to get killed if it exceeds 3 GB per core/slot -- 
-#BSUB -M 3GB
-### -- set walltime limit: hh:mm -- 
-#BSUB -W 24:00 
-### -- set the email address -- 
+### –- specify queue --
+#BSUB -q gpuv100
+### -- set the job Name --
+#BSUB -J CNN10_5
+### -- ask for number of cores (default: 1) --
+#BSUB -n 1
+### -- Select the resources: 1 gpu in exclusive process mode --
+#BSUB -gpu "num=1:mode=exclusive_process"
+### -- set walltime limit: hh:mm --  maximum 24 hours for GPU-queues right now
+#BSUB -W 24:00
+# request 5GB of system-memory
+#BSUB -R "rusage[mem=5GB]"
+### -- set the email address --
 # please uncomment the following line and put in your e-mail address,
 # if you want to receive e-mail notifications on a non-default address
 #BSUB -u jorge.sintes@gmail.com
-### -- send notification at start -- 
-#BSUB -B 
-### -- send notification at completion -- 
-#BSUB -N 
-### -- Specify the output and error file. %J is the job-id -- 
-### -- -o and -e mean append, -oo and -eo mean overwrite -- 
+### -- send notification at start --
+#BSUB -B
+### -- send notification at completion--
+#BSUB -N
+### -- Specify the output and error file. %J is the job-id --
+### -- -o and -e mean append, -oo and -eo mean overwrite --
 #BSUB -o Output_%J.out 
 #BSUB -e Error_%J.err 
 
